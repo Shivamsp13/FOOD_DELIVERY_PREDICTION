7401fe3db6d1306e8a7564dae1e6efc6f8bb7c1f
Shivam Pandey <shivamofficialsp13@gmail.com>
2025-12-15 17:49:09 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -46,13 +46,3 @@ class DataIngestion:
         except Exception as e:
             logging.info("Erro occured in data ingestion stage")
             raise CustomException(e, sys)
-
-if __name__ =="__main__":
-    obj = DataIngestion()
-    train_data_path , test_data_path = obj.initiate_data_ingestion()
-
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path , test_data_path)
-
-    modeltrainer = ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
