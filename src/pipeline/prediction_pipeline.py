2bbc3c5eedbc6f4731010bb903ad58b85d60790a
Shivam Pandey <shivamofficialsp13@gmail.com>
2025-12-15 23:56:01 +0530

    Update prediction_pipeline.py

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -80,23 +80,19 @@ class CustomClass:
                  preference_p2: str,
                  reviews: str = ""):
         
-        # Numerical features
         self.age = age
         self.family_size = family_size
         
-        # Nominal features
         self.gender = gender
         self.marital_status = marital_status
         self.order_time = order_time
         self.preference_p1 = preference_p1
         self.preference_p2 = preference_p2
         
-        # Ordinal features
         self.occupation = occupation
         self.monthly_income = monthly_income
         self.educational_qualifications = educational_qualifications
         
-        # Likert scale features (ordinal)
         self.ease_convenient = ease_convenient
         self.time_saving = time_saving
         self.more_restaurant_choices = more_restaurant_choices
@@ -122,15 +118,11 @@ class CustomClass:
         self.good_road_condition = good_road_condition
         self.low_quantity_low_time = low_quantity_low_time
         self.delivery_person_ability = delivery_person_ability
-        
-        # Yes/No/Maybe features
         self.influence_time = influence_time
         self.influence_rating = influence_rating
         
-        # Wait time feature
         self.maximum_wait_time = maximum_wait_time
         
-        # Importance scale features
         self.less_delivery_time = less_delivery_time
         self.high_quality_package = high_quality_package
         self.number_of_calls = number_of_calls
@@ -140,29 +132,28 @@ class CustomClass:
         self.good_taste = good_taste
         self.good_quantity = good_quantity
         
-        # Text feature for sentiment analysis
         self.reviews = reviews
     
     def get_data_DataFrame(self):
         try:
             custom_input = {
-                # Numerical features
+
                 "Age": [self.age],
                 "Family size": [self.family_size],
                 
-                # Nominal features
+
                 "Gender": [self.gender],
                 "Marital Status": [self.marital_status],
                 "Order Time": [self.order_time],
                 "Perference(P1)": [self.preference_p1],
                 "Perference(P2)": [self.preference_p2],
                 
-                # Ordinal features
+
                 "Occupation": [self.occupation],
                 "Monthly Income": [self.monthly_income],
                 "Educational Qualifications": [self.educational_qualifications],
                 
-                # Likert scale features
+
                 "Ease and convenient": [self.ease_convenient],
                 "Time saving": [self.time_saving],
                 "More restaurant choices": [self.more_restaurant_choices],
@@ -189,14 +180,14 @@ class CustomClass:
                 "Low quantity low time": [self.low_quantity_low_time],
                 "Delivery person ability": [self.delivery_person_ability],
                 
-                # Yes/No/Maybe features
+
                 "Influence of time": [self.influence_time],
                 "Influence of rating": [self.influence_rating],
                 
-                # Wait time feature
+
                 "Maximum wait time": [self.maximum_wait_time],
                 
-                # Importance scale features
+
                 "Less Delivery time": [self.less_delivery_time],
                 "High Quality of package": [self.high_quality_package],
                 "Number of calls": [self.number_of_calls],
@@ -206,7 +197,7 @@ class CustomClass:
                 "Good Taste ": [self.good_taste],
                 "Good Quantity": [self.good_quantity],
                 
-                # Text feature for sentiment analysis
+
                 "Reviews": [self.reviews]
             }
             
