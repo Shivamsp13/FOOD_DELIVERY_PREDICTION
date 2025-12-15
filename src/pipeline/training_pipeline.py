a29f56e3b63c6244e2d1d20ea3ef76b6529e3021
Shivam Pandey <shivamofficialsp13@gmail.com>
2025-12-15 17:23:53 +0530

    Update training_pipeline.py

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -13,5 +13,3 @@ if __name__ == "__main__":
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
     model_training = ModelTrainer()
     model_training.inititate_model_trainer(train_arr, test_arr)
-
-    # src\pipeline\training_pipeline.py
